ed275fa08fe9f469b5fb23d4ba715271a66d5744
Olena <code@nightsnake.maskmy.id>
2024-01-11 20:44:44 +0200

    add exercise data

---
+++ ex1.py
@@ -0,0 +1,6 @@
+# a*x + b = 0 (a!=0)
+a = float(input("a: "))
+b = float(input("b: "))
+print(f"{a}*x + {b}=01")
+x = -b / a
+print(f"x = {x}")
---
+++ ex2.py
@@ -0,0 +1,5 @@
+a = float(input("a: "))
+b = float(input("b: "))
+print(f"a = {a}, b = {b}")
+c = (a**2 + b**2) ** 0.5
+print(f"c={c}")
